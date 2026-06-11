85c6a4f310dd39967b7dd2df8b4cd17fa9c5bad8
DLC86 <tone.scientist.yt@gmail.com>
2026-06-11 22:11:25 +0200

    Add anti-alias filtering options for NAM oversampling

--- dsp/ResamplingContainer/ResamplingContainer.h
+++ dsp/ResamplingContainer/ResamplingContainer.h
@@ -47,6 +47,8 @@ iPlug 2 includes the following 3rd party libraries (see each license info):
 #include <iostream>
 #include <functional>
 #include <cmath>
+#include <algorithm>
+#include <vector>
 
 // #include "IPlugPlatform.h"
 
@@ -57,6 +59,12 @@ iPlug 2 includes the following 3rd party libraries (see each license info):
 
 namespace dsp
 {
+
+enum class EAntiAliasFilterPhase
+{
+  MinimumPhase = 0,
+  LinearPhase
+};
 
 /** A multi-channel real-time resampling container that can be used to resample
  * audio processing to a specified sample rate for the situation where you have
@@ -89,20 +97,26 @@ public:
   using LanczosResampler = LanczosResampler<T, NCHANS, A>;
 
   // :param renderingSampleRate: The sample rate required by the code to be encapsulated.
-  ResamplingContainer(double renderingSampleRate)
+  ResamplingContainer(double renderingSampleRate, EAntiAliasFilterPhase filterPhase = EAntiAliasFilterPhase::LinearPhase)
   : mRenderingSampleRate(renderingSampleRate)
+  , mFilterPhase(filterPhase)
   {
   }
 
   ResamplingContainer(const ResamplingContainer&) = delete;
   ResamplingContainer& operator=(const ResamplingContainer&) = delete;
 
+  void SetAntiAliasFilterPhase(EAntiAliasFilterPhase filterPhase)
+  {
+    mFilterPhase = filterPhase;
+  }
+
   // :param inputSampleRate: The external sample rate interacting with this object.
   // :param blockSize: The largest block size that will be given to this class to process until Reset()  is called
   //     again.
   void Reset(double inputSampleRate, int blockSize = DEFAULT_BLOCK_SIZE)
   {
-    if (mInputSampleRate == inputSampleRate && mMaxBlockSize == blockSize)
+    if (mInputSampleRate == inputSampleRate && mMaxBlockSize == blockSize && mDesignedFilterPhase == mFilterPhase)
     {
       ClearBuffers();
       return;
@@ -111,6 +125,11 @@ public:
     mInputSampleRate = inputSampleRate;
     mRatio1 = mInputSampleRate / mRenderingSampleRate;
     mRatio2 = mRenderingSampleRate / mInputSampleRate;
+    const double exactDownsampleFactor = mRenderingSampleRate / mInputSampleRate;
+    mIntegerDownsampleFactor = static_cast<int>(std::round(exactDownsampleFactor));
+    mUseIntegerDownsampler =
+      mIntegerDownsampleFactor > 1 && std::abs(exactDownsampleFactor - mIntegerDownsampleFactor) < 1.0e-9;
+    mDecimationPhase = 0;
     // The buffers for the encapsulated code need to be long enough to hold the correesponding number of samples
     mMaxBlockSize = blockSize;
     mMaxEncapsulatedBlockSize = MaxEncapsulatedBlockSize(blockSize);
@@ -118,45 +137,60 @@ public:
     mScratchExternalInputData.Resize(mMaxBlockSize * NCHANS); // This may contain junk right now.
     mEncapsulatedInputData.Resize(mMaxEncapsulatedBlockSize * NCHANS); // This may contain junk right now.
     mEncapsulatedOutputData.Resize(mMaxEncapsulatedBlockSize * NCHANS); // This may contain junk right now.
+    mAntiAliasOutputData.Resize(mMaxEncapsulatedBlockSize * NCHANS); // This may contain junk right now.
     mScratchExternalInputPointers.Empty();
     mEncapsulatedInputPointers.Empty();
     mEncapsulatedOutputPointers.Empty();
+    mAntiAliasOutputPointers.Empty();
 
     for (auto chan = 0; chan < NCHANS; chan++)
     {
       mScratchExternalInputPointers.Add(mScratchExternalInputData.Get() + (chan * mMaxBlockSize));
       mEncapsulatedInputPointers.Add(mEncapsulatedInputData.Get() + (chan * mMaxEncapsulatedBlockSize));
       mEncapsulatedOutputPointers.Add(mEncapsulatedOutputData.Get() + (chan * mMaxEncapsulatedBlockSize));
+      mAntiAliasOutputPointers.Add(mAntiAliasOutputData.Get() + (chan * mMaxEncapsulatedBlockSize));
     }
 
+    DesignAntiAliasFilter();
+
+    mResampler1 = std::make_unique<LanczosResampler>(mInputSampleRate, mRenderingSampleRate);
+    mResampler2 = mUseIntegerDownsampler ? nullptr
+                                         : std::make_unique<LanczosResampler>(mRenderingSampleRate, mInputSampleRate);
+
+    // Zeroes the scratch pointers so that we warm up with silence.
+    ClearBuffers();
+
+    // Warm up the resampling container with enough silence that the first real buffer can yield the required number
+    // of output samples.
+    const auto midSamples =
+      mUseIntegerDownsampler ? static_cast<size_t>(mIntegerDownsampleFactor) : mResampler2->GetNumSamplesRequiredFor(1);
+    mLatency = int(mResampler1->GetNumSamplesRequiredFor(midSamples));
+    if (mAntiAliasEnabled && mFilterPhase == EAntiAliasFilterPhase::LinearPhase)
+      mLatency += 64;
+    // 1. Push some silence through the first resampler.
+    mResampler1->PushBlock(mScratchExternalInputPointers.GetList(), mLatency);
+    const size_t populated = mResampler1->PopBlock(mEncapsulatedInputPointers.GetList(), midSamples);
+    if (populated < midSamples)
     {
-      mResampler1 = std::make_unique<LanczosResampler>(mInputSampleRate, mRenderingSampleRate);
-      mResampler2 = std::make_unique<LanczosResampler>(mRenderingSampleRate, mInputSampleRate);
-
-      // Zeroes the scratch pointers so that we warm up with silence.
-      ClearBuffers();
-
-      // Warm up the resampling container with enough silence that the first real buffer can yield the required number
-      // of output samples.
-      const auto midSamples = mResampler2->GetNumSamplesRequiredFor(1);
-      mLatency = int(mResampler1->GetNumSamplesRequiredFor(midSamples));
-      // 1. Push some silence through the first resampler.
-      //
-      mResampler1->PushBlock(mScratchExternalInputPointers.GetList(), mLatency);
-      const size_t populated = mResampler1->PopBlock(mEncapsulatedInputPointers.GetList(), midSamples);
-      if (populated < midSamples)
-      {
-        throw std::runtime_error("Didn't get enough samples required for pre-population!");
-      }
-      // 2. "process" the warm-up in the encapsulated DSP.
-      // Since this is an audio effect, we can assume that (1) it's causal and (2) that it's silent until
-      // a non-silent input is given to it.
-      // Therefore, we don't *acutally* need to use `func()`--we can assume that it would output silence!
-      // func(mEncapsulatedInputPointers.GetList(), mEncapsulatedOutputPointers.GetList(), (int)populated);
-      FallbackFunc(mEncapsulatedInputPointers.GetList(), mEncapsulatedOutputPointers.GetList(), (int)populated);
-      mResampler2->PushBlock(mEncapsulatedOutputPointers.GetList(), populated);
-      // Now we're ready for the first "real" buffer.
+      throw std::runtime_error("Didn't get enough samples required for pre-population!");
     }
+    // 2. "process" the warm-up in the encapsulated DSP.
+    // Since this is an audio effect, we can assume that (1) it's causal and (2) that it's silent until
+    // a non-silent input is given to it.
+    // Therefore, we don't *acutally* need to use `func()`--we can assume that it would output silence!
+    // func(mEncapsulatedInputPointers.GetList(), mEncapsulatedOutputPointers.GetList(), (int)populated);
+    FallbackFunc(mEncapsulatedInputPointers.GetList(), mEncapsulatedOutputPointers.GetList(), (int)populated);
+    T** resamplerInput = mEncapsulatedOutputPointers.GetList();
+    if (mAntiAliasEnabled)
+    {
+      ApplyAntiAliasFilter(populated);
+      resamplerInput = mAntiAliasOutputPointers.GetList();
+    }
+    if (mUseIntegerDownsampler)
+      DirectDecimateBlock(resamplerInput, populated, nullptr, 0);
+    else
+      mResampler2->PushBlock(resamplerInput, populated);
+    // Now we're ready for the first "real" buffer.
   }
 
   /** Resample an input block with a per-block function (up sample input -> process with function -> down sample)
@@ -168,6 +202,7 @@ public:
   void ProcessBlock(T** inputs, T** outputs, int nFrames, BlockProcessFunc func)
   {
     mResampler1->PushBlock(inputs, nFrames);
+    mOutputWritePos = 0;
     // This is the most samples the encapsualted context might get. Sometimes it'll get fewer.
     const auto maxEncapsulatedLen = MaxEncapsulatedBlockSize(nFrames);
 
@@ -184,11 +219,20 @@ public:
       }
       func(mEncapsulatedInputPointers.GetList(), mEncapsulatedOutputPointers.GetList(), (int)populated1);
       // And push the results into the second resampler so that it has what the external context requires.
-      mResampler2->PushBlock(mEncapsulatedOutputPointers.GetList(), populated1);
+      T** resamplerInput = mEncapsulatedOutputPointers.GetList();
+      if (mAntiAliasEnabled)
+      {
+        ApplyAntiAliasFilter(populated1);
+        resamplerInput = mAntiAliasOutputPointers.GetList();
+      }
+      if (mUseIntegerDownsampler)
+        DirectDecimateBlock(resamplerInput, populated1, outputs, nFrames);
+      else
+        mResampler2->PushBlock(resamplerInput, populated1);
     }
 
     // Pop the required output from the second resampler for the external context.
-    const auto populated2 = mResampler2->PopBlock(outputs, nFrames);
+    const auto populated2 = mUseIntegerDownsampler ? mOutputWritePos : mResampler2->PopBlock(outputs, nFrames);
     if (populated2 < nFrames)
     {
       std::cerr << "Did not yield enough samples (" << populated2 << ") to provide the required output buffer (expected"
@@ -204,7 +248,8 @@ public:
     }
     // Get ready for the next block:
     mResampler1->RenormalizePhases();
-    mResampler2->RenormalizePhases();
+    if (mResampler2 != nullptr)
+      mResampler2->RenormalizePhases();
   }
 
   int GetLatency() const { return mLatency; }
@@ -279,6 +324,8 @@ private:
     const auto encapsulatedDataSize = DataSize(mMaxEncapsulatedBlockSize);
     memset(mEncapsulatedInputData.Get(), 0.0f, encapsulatedDataSize);
     memset(mEncapsulatedOutputData.Get(), 0.0f, encapsulatedDataSize);
+    memset(mAntiAliasOutputData.Get(), 0.0f, encapsulatedDataSize);
+    std::fill(mAntiAliasHistory.begin(), mAntiAliasHistory.end(), T(0.0));
 
     if (mResampler1 != nullptr)
     {
@@ -288,6 +335,8 @@ private:
     {
       mResampler2->ClearBuffer();
     }
+    mDecimationPhase = 0;
+    mOutputWritePos = 0;
   }
 
   // How big could the corresponding encapsulated buffer be for a buffer at the external sample rate of a given size?
@@ -303,7 +352,188 @@ private:
   {
     for (int i = 0; i < NCHANS; i++)
     {
-      memcpy(inputs[i], outputs[i], n * sizeof(T));
+      memcpy(outputs[i], inputs[i], n * sizeof(T));
+    }
+  }
+
+  void DesignAntiAliasFilter()
+  {
+    mAntiAliasEnabled = mRenderingSampleRate > (mInputSampleRate * 1.000001);
+    mAntiAliasCoefficients.clear();
+    mAntiAliasHistory.clear();
+    mMinimumPhaseSections.clear();
+    mMinimumPhaseState.clear();
+
+    if (!mAntiAliasEnabled)
+    {
+      mDesignedFilterPhase = mFilterPhase;
+      return;
+    }
+
+    if (mFilterPhase == EAntiAliasFilterPhase::MinimumPhase)
+    {
+      DesignMinimumPhaseAntiAliasFilter();
+      mDesignedFilterPhase = mFilterPhase;
+      return;
+    }
+
+    DesignLinearPhaseAntiAliasFilter();
+    mDesignedFilterPhase = mFilterPhase;
+  }
+
+  void DesignLinearPhaseAntiAliasFilter()
+  {
+    constexpr double pi = 3.14159265358979323846264338327950288;
+    constexpr int numTaps = 255;
+    const double cutoff = std::min(0.49, 0.475 * mInputSampleRate / mRenderingSampleRate);
+    double sum = 0.0;
+
+    mAntiAliasCoefficients.resize(numTaps);
+    for (int n = 0; n < numTaps; n++)
+    {
+      const double x = n - 0.5 * (numTaps - 1);
+      const double sinc = std::abs(x) < 1.0e-12 ? 2.0 * cutoff : std::sin(2.0 * pi * cutoff * x) / (pi * x);
+      const double window = 0.42 - 0.5 * std::cos(2.0 * pi * n / (numTaps - 1))
+                            + 0.08 * std::cos(4.0 * pi * n / (numTaps - 1));
+      mAntiAliasCoefficients[n] = static_cast<T>(sinc * window);
+      sum += mAntiAliasCoefficients[n];
+    }
+
+    if (std::abs(sum) > 1.0e-20)
+      for (auto& c : mAntiAliasCoefficients)
+        c = static_cast<T>(c / sum);
+
+    mAntiAliasHistory.assign(NCHANS * (numTaps - 1), T(0.0));
+  }
+
+  void ApplyAntiAliasFilter(size_t nFrames)
+  {
+    if (mFilterPhase == EAntiAliasFilterPhase::MinimumPhase)
+      ApplyMinimumPhaseAntiAliasFilter(nFrames);
+    else
+      ApplyLinearPhaseAntiAliasFilter(nFrames);
+  }
+
+  void ApplyLinearPhaseAntiAliasFilter(size_t nFrames)
+  {
+    const size_t numTaps = mAntiAliasCoefficients.size();
+    const size_t historyLen = numTaps - 1;
+
+    for (int chan = 0; chan < NCHANS; chan++)
+    {
+      T* input = mEncapsulatedOutputPointers.Get(chan);
+      T* output = mAntiAliasOutputPointers.Get(chan);
+      T* history = mAntiAliasHistory.data() + chan * historyLen;
+
+      for (size_t s = 0; s < nFrames; s++)
+      {
+        T y = T(0.0);
+        for (size_t k = 0; k < numTaps; k++)
+        {
+          const long inputIndex = static_cast<long>(s) - static_cast<long>(k);
+          const T x = inputIndex >= 0 ? input[inputIndex] : history[historyLen + inputIndex];
+          y += mAntiAliasCoefficients[k] * x;
+        }
+        output[s] = y;
+      }
+
+      if (nFrames >= historyLen)
+      {
+        std::copy(input + nFrames - historyLen, input + nFrames, history);
+      }
+      else
+      {
+        std::move(history + nFrames, history + historyLen, history);
+        std::copy(input, input + nFrames, history + historyLen - nFrames);
+      }
+    }
+  }
+
+  struct BiquadCoefficients
+  {
+    T b0 = T(1.0);
+    T b1 = T(0.0);
+    T b2 = T(0.0);
+    T a1 = T(0.0);
+    T a2 = T(0.0);
+  };
+
+  struct BiquadState
+  {
+    T z1 = T(0.0);
+    T z2 = T(0.0);
+  };
+
+  void DesignMinimumPhaseAntiAliasFilter()
+  {
+    constexpr double pi = 3.14159265358979323846264338327950288;
+    constexpr int order = 12;
+    constexpr int numSections = order / 2;
+    const double cutoffHz = 0.475 * mInputSampleRate;
+    const double cutoff = std::min(0.49, cutoffHz / mRenderingSampleRate);
+    const double omega = 2.0 * pi * cutoff;
+    const double sinOmega = std::sin(omega);
+    const double cosOmega = std::cos(omega);
+
+    mMinimumPhaseSections.resize(numSections);
+    for (int section = 0; section < numSections; section++)
+    {
+      const double q = 1.0 / (2.0 * std::cos((2.0 * section + 1.0) * pi / (2.0 * order)));
+      const double alpha = sinOmega / (2.0 * q);
+      const double a0 = 1.0 + alpha;
+
+      BiquadCoefficients coeffs;
+      coeffs.b0 = static_cast<T>((1.0 - cosOmega) * 0.5 / a0);
+      coeffs.b1 = static_cast<T>((1.0 - cosOmega) / a0);
+      coeffs.b2 = static_cast<T>((1.0 - cosOmega) * 0.5 / a0);
+      coeffs.a1 = static_cast<T>((-2.0 * cosOmega) / a0);
+      coeffs.a2 = static_cast<T>((1.0 - alpha) / a0);
+      mMinimumPhaseSections[section] = coeffs;
+    }
+
+    mMinimumPhaseState.assign(NCHANS * mMinimumPhaseSections.size(), BiquadState {});
+  }
+
+  void ApplyMinimumPhaseAntiAliasFilter(size_t nFrames)
+  {
+    for (int chan = 0; chan < NCHANS; chan++)
+    {
+      T* input = mEncapsulatedOutputPointers.Get(chan);
+      T* output = mAntiAliasOutputPointers.Get(chan);
+
+      for (size_t s = 0; s < nFrames; s++)
+      {
+        T y = input[s];
+        for (size_t section = 0; section < mMinimumPhaseSections.size(); section++)
+        {
+          const auto& coeffs = mMinimumPhaseSections[section];
+          auto& state = mMinimumPhaseState[chan * mMinimumPhaseSections.size() + section];
+          const T out = coeffs.b0 * y + state.z1;
+          state.z1 = coeffs.b1 * y - coeffs.a1 * out + state.z2;
+          state.z2 = coeffs.b2 * y - coeffs.a2 * out;
+          y = out;
+        }
+        output[s] = y;
+      }
+    }
+  }
+
+  void DirectDecimateBlock(T** input, size_t nFrames, T** outputs, int maxOutputFrames)
+  {
+    for (size_t s = 0; s < nFrames; s++)
+    {
+      if (mDecimationPhase == 0)
+      {
+        if (outputs != nullptr && mOutputWritePos < static_cast<size_t>(maxOutputFrames))
+        {
+          for (int chan = 0; chan < NCHANS; chan++)
+            outputs[chan][mOutputWritePos] = input[chan][s];
+          mOutputWritePos++;
+        }
+      }
+      mDecimationPhase++;
+      if (mDecimationPhase >= mIntegerDownsampleFactor)
+        mDecimationPhase = 0;
     }
   }
 
@@ -315,6 +545,19 @@ private:
   WDL_PtrList<T> mEncapsulatedInputPointers;
   WDL_TypedBuf<T> mEncapsulatedOutputData;
   WDL_PtrList<T> mEncapsulatedOutputPointers;
+  WDL_TypedBuf<T> mAntiAliasOutputData;
+  WDL_PtrList<T> mAntiAliasOutputPointers;
+  std::vector<T> mAntiAliasCoefficients;
+  std::vector<T> mAntiAliasHistory;
+  std::vector<BiquadCoefficients> mMinimumPhaseSections;
+  std::vector<BiquadState> mMinimumPhaseState;
+  bool mAntiAliasEnabled = false;
+  EAntiAliasFilterPhase mFilterPhase = EAntiAliasFilterPhase::LinearPhase;
+  EAntiAliasFilterPhase mDesignedFilterPhase = EAntiAliasFilterPhase::LinearPhase;
+  bool mUseIntegerDownsampler = false;
+  int mIntegerDownsampleFactor = 1;
+  int mDecimationPhase = 0;
+  size_t mOutputWritePos = 0;
   // Sample rate ratio from external to encapsulated, from encapsulated to external.
   double mRatio1 = 0.0, mRatio2 = 0.0;
   // Sample rate of the external context.
